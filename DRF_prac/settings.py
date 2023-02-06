1c413c50e0763e5c601784d9c1b076a51e4d508c
GAEUL MOON <103443650+gaeull@users.noreply.github.com>
2023-02-06 14:22:27 +0900

    Update settings.py

--- DRF_prac/settings.py
+++ DRF_prac/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d^sgow-%%ub*^ak_k-n(n#qo4e_isuv$=@b)ol*9bt3y_g*dik'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
